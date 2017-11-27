d71e76abdd696024b484654299421b61efd8bb37
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-11-27 16:25:29 +0100

    Added interface that will compile into different things depending on std is used or not

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+extern crate embedded_types;
+
 #[macro_use]
 mod control_table;
 
 pub mod protocol2;
 pub mod pro;
 
+#[cfg(not(feature = "std"))]
+pub trait Interface : ::embedded_types::io::Read + ::embedded_types::io::Write {}
+
+#[cfg(feature = "std")]
+pub trait Interface : ::std::io::BufRead + ::std::io::Write {}
 
 #[cfg(test)]
 mod tests {
